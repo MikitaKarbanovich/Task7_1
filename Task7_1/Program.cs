4015b59fc6ee9323528ea767c98908be95df895b
MikitaKarbanovich <nikitakarbanovich@gmail.com>
2016-10-07 11:52:11 +0300

    Add project files.

---
+++ Task7_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                WorkWithFile wwf = new WorkWithFile();
+                System.Console.WriteLine("Enter the path, where you want to create the folder.");
+                wwf.SetPath();
+                wwf.ReadFromFile();
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: something is broken", e.Message);
+                Log(ioEx.Message);
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
---
+++ Task7_1/WorkWithFile.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7_1
+{
+    public class WorkWithFile
+    {
+        string pathToReadFile = string.Empty;
+        public void SetPath()
+        {
+            string keyboardInput = Console.ReadLine();
+            while (!File.Exists(keyboardInput))
+            {
+                Console.WriteLine("Bad path, try again.");
+                Log($"Error: File isn't found by path { keyboardInput}.\n");
+                keyboardInput = Console.ReadLine();
+            }
+            this.pathToReadFile = keyboardInput;
+        }
+        public void ReadFromFile()
+        {
+            try
+            {
+                StreamReader fs = new StreamReader(pathToReadFile);
+                string temp = String.Empty;
+                int stringCounter = 0;
+                while (fs.ReadLine() != null)
+                {
+                    stringCounter++;
+                    temp = fs.ReadLine();
+                    if (!temp.Equals(String.Empty))
+                    {
+                        Console.WriteLine(temp.First());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Empty string.");
+                        Log($"Error: Empty string №{ stringCounter}.\n");
+                    }
+                }
+
+            }
+            catch (FileNotFoundException ioEx)
+            {
+                Console.WriteLine(ioEx.Message);
+                Log(ioEx.Message);
+            }
+        }
+        public static void Log(string message)
+        {
+            DateTime localDate = DateTime.Now;
+            string PATHTOLOG = @"D:\logger.txt";
+            File.AppendAllText(PATHTOLOG, "[" +
+                localDate.ToString("G", CultureInfo.CreateSpecificCulture("ru-RU")) +
+                "] " +
+                message);
+        }
+    }
+}
